12321cd0e2216cdf4ab1f5b9accb19a180386583
Victoria Raquel Ferreira <v.raquelferreira@bol.com.br>
2021-05-15 19:00:53 -0300

    Create  1007Diferença.c

---
+++ 1007Diferença.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  int A,B,C,D,final;
+  
+  scanf("%i %i %i %i", &A, &B, &C, &D);
+  final = (( A * B)- (C * D));
+  printf("DIFERENCA = %i\n", final);
+ 
+    return 0; 
+}
